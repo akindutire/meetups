e1aa48d686be8d7a0a23b63b15fb1d25bea1aa8c
akindutire <akinsamuel33@gmail.com>
2021-12-18 17:36:12 +0100

    added password reset, user reg MS

---
+++ backend/password.reset/src/main/kotlin/com/meet/user/password/reset/PasswordResetApplication.kt
@@ -0,0 +1,13 @@
+package com.meet.user.password.reset
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
+@SpringBootApplication
+@EnableEurekaClient
+class PasswordResetApplication
+
+fun main(args: Array<String>) {
+	runApplication<PasswordResetApplication>(*args)
+}
--- backend/user.registration/src/main/kotlin/com/meet/user/registration/controller/api/RegistrationCtrl.kt
+++ backend/user.registration/src/main/kotlin/com/meet/user/registration/controller/api/RegistrationCtrl.kt
@@ -3,15 +3,23 @@ package com.meet.user.registration.controller.api
 import com.meet.user.registration.request.core.RegistrationReq
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
 @RequestMapping("/join/")
 class RegistrationCtrl {
+
+    @GetMapping
+    fun welcome(@RequestBody registrationReq: RegistrationReq) : ResponseEntity<Any> {
+
+        val map = HashMap<String, String?>()
+        map.put("data", "Active")
+
+        val res = ResponseEntity<Any>(map, HttpStatus.OK);
+
+        return res
+    }
 
     @PostMapping("m")
     fun join(@RequestBody registrationReq: RegistrationReq) : ResponseEntity<Any> {
