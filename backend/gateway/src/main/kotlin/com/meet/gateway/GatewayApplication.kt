8b40e563b4926403913ab751ab69d31e58c6316c
akindutire <akinsamuel33@gmail.com>
2022-01-03 14:32:59 +0100

    spring cloud bus integration

--- backend/gateway/src/main/kotlin/com/meet/gateway/GatewayApplication.kt
+++ backend/gateway/src/main/kotlin/com/meet/gateway/GatewayApplication.kt
@@ -2,11 +2,13 @@ package com.meet.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 class GatewayApplication
 
 fun main(args: Array<String>) {
--- backend/user.registration/src/main/kotlin/com/meet/user/registration/UserRegistrationApplication.kt
+++ backend/user.registration/src/main/kotlin/com/meet/user/registration/UserRegistrationApplication.kt
@@ -2,10 +2,12 @@ package com.meet.user.registration
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 class UserRegistrationApplication
 
 fun main(args: Array<String>) {
