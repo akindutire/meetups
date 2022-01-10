cbf7129bf10ad84138ed6b63f65ecaeb0d8466cd
akindutire <akinsamuel33@gmail.com>
2022-01-10 15:15:21 +0100

    user profile microservice

--- backend/profile/src/main/kotlin/com/meet/profile/ProfileApplication.kt
+++ backend/profile/src/main/kotlin/com/meet/profile/ProfileApplication.kt
@@ -2,8 +2,12 @@ package com.meet.profile
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
+@RefreshScope
 class ProfileApplication
 
 fun main(args: Array<String>) {
